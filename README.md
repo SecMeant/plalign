f99490bec606f603ffc4b63d6a52ccf62a70346c
SecMeant <secmeant@gmail.com>
2024-03-24 18:15:24 +0100

    Add initial version of the script

---
+++ README.md
@@ -0,0 +1,7 @@
+# plalign - Photolithography align
+
+A tool for silicon wafer alignment before photolithography step for very obscure DIY setup.
+
+## Dependencies
+
+    1. OpenCV (python-opencv)
---
+++ plalign.py
@@ -0,0 +1,75 @@
+#!/bin/python
+
+import cv2
+import os
+import time
+import sys
+import select
+
+merge_ratio = 0.75
+merge_inc = 0.05
+
+cam = cv2.VideoCapture("/dev/video0")
+
+def genoverlay(fg, bg, ratio):
+    if fg is None or bg is None:
+        return None
+
+    return cv2.addWeighted(fg, ratio, bg, 1.0-ratio, 0)
+
+fg = None
+bg = None
+while True:
+    choice = cv2.waitKey(50)
+    if choice != -1:
+        choice = chr(choice & 0x7f)
+
+    # Background pic
+    if choice == 'b':
+
+        ret, frame = cam.read()
+
+        if ret:
+            bg = frame
+            #cv2.imshow('bg', bg)
+        else:
+            print('Failed to take screenshot')
+
+        continue
+
+    elif choice == 'k':
+        merge_ratio += merge_inc
+        if merge_ratio > 1.0:
+            merge_raio = 1.0
+
+        continue
+
+    elif choice == 'j':
+        merge_ratio -= merge_inc
+        if merge_ratio < 0.0:
+            merge_ratio = 0.0
+
+        continue
+
+    # Exit
+    elif choice == 'q' or choice == 'x':
+        break
+
+    # Foreground pic
+    else:
+
+        ret, frame = cam.read()
+
+        if ret:
+            fg = frame
+            overlay = genoverlay(fg, bg, merge_ratio)
+            if overlay is None:
+                cv2.imshow('overlay', overlay)
+            else:
+                cv2.imshow('overlay', fg)
+
+        else:
+            print('Failed to render overlay')
+
+cam.release()
+cv2.destroyAllWindows()
